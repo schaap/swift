1fef19c5f976af0d2346fe129ad2bf1f6aaaf6f1
Thomas Schaap <thomas@ch.tudelft.nl>
2011-08-10 14:54:42 +0200

    Added a statistics thread to the swift test environment program.

--- testenvironment/swift.cpp
+++ testenvironment/swift.cpp
@@ -15,6 +15,10 @@
 #include <getopt.h>
 #include <errno.h>
 #include <signal.h>
+#include <time.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <pthread.h>
 
 #include "compat.h"
 #include "swift.h"
@@ -48,6 +52,225 @@ int readPositiveIntArg( ) {
     return val;
 }
 
+// statargs should be of type char**[] with *(statargs[0]) being the filename to write
+// statistics to and *(statargs[1]) being the name of the interface to watch.
+void* statisticsThread( void* statargs ) {
+    // The statistics file is a (semi-)binary data file containing a human readable
+    // date marker on the first line, the version (an integer) on the second line
+    // and statistics data after that. Lines are separated by '\n'. Data records
+    // are described per version and are appended without extra newline. Any
+    // newlines 'between' records are actually part of the records themselves.
+    //
+    // The version of the statistics is written to the file and should be used
+    // to verify how the data is encoded. Below a list of versions that have been
+    // used and are in use.
+    //
+    // Note on hex|64: this encoding was chosen because it allows checking the
+    // data for obvious corruptions, avoids control characters, and can still be
+    // encoded efficiently. The actual encoding: convert to big endian byte order
+    // and store each byte X of the data as two bytes: ((X >> 4) | 64) and
+    // ((X & 15) | 64).
+    //
+    // Version history (only append, DO NOT delete or modify old versions):
+    // - 1:
+    // -- 1 byte 'a'
+    // -- 32 bits microseconds user CPU time (hex|64 encoded)
+    // -- 1 byte 'b'
+    // -- 32 bits microseconds kernel CPU time (hex|64 encoded)
+    // -- 1 byte 'c'
+    // -- 32 bits number of bytes read on watched interface (hex|64 encoded)
+    // -- 1 byte 'd'
+    // -- 32 bits number of bytes writte to watched interface (hex|64 encoded)
+    // -- 1 byte 'e'
+    // -- 16 bits number of microseconds slept during last period (hex|64 encoded)
+    // -- 1 byte '\n'
+    
+    // Extract parameters
+    char* filename = *(((char***)statargs)[0]);
+    char* iface = *(((char***)statargs)[1]);
+    
+    // Prepare all measurement tools
+    struct rusage ru1;
+    struct rusage ru2;
+    long rx1, rx2, tx1, tx2;
+    char buf[1024];
+    char buf2[1024];
+    ssize_t n;
+    struct timeval tv1;
+    struct timeval tv2;
+    uint32_t microsuser, microskernel, bytesread, byteswritten;
+    useconds_t sleepytime;
+    uint16_t sleepytime16;
+    char* microsuser_p = (char*)&microsuser;
+    char* microskernel_p = (char*)&microskernel;
+    char* bytesread_p = (char*)&bytesread;
+    char* byteswritten_p = (char*)&byteswritten;
+    char* sleepytime16_p = (char*)&sleepytime16;
+
+    int statfile = open( filename, O_APPEND|O_CREAT|O_WRONLY, S_IRUSR|S_IWUSR );
+    if( statfile < 0 ) {
+        print_error( "Could not open statistics file" );
+        exit( -1 );
+    }
+
+    char rxfilename[1024] = "/sys/class/net/";
+    char txfilename[1024];
+    strncat( rxfilename + strlen(rxfilename), iface, 1023-strlen(rxfilename) );
+    strncpy( txfilename, rxfilename, 1024 );
+    strncat( rxfilename + strlen(rxfilename), "/statistics/rx_bytes", 1023-strlen(rxfilename) );
+    strncat( txfilename + strlen(txfilename), "/statistics/tx_bytes", 1023-strlen(txfilename) );
+
+    int rxfile = open( rxfilename, O_RDONLY );
+    if( rxfile < 0 ) {
+        fprintf( stderr, "%s\n", rxfilename );
+        print_error( "Could not open read statistics for interface" );
+        exit( -1 );
+    }
+    int txfile = open( txfilename, O_RDONLY );
+    if( txfile < 0 ) {
+        print_error( "Could not open write statistics for interface" );
+        exit( -1 );
+    }
+
+    time_t now;
+    time( &now );
+    ctime_r( &now, buf2 );
+
+    // Take first measures
+    if( getrusage( RUSAGE_SELF, &ru1 ) ) {
+        print_error( "getrusage failed" );
+        exit( -1 );
+    }
+    if( ( n = pread( rxfile, buf, 1023, 0 ) ) < 0 ) {
+        print_error( "Could not read from read statistics for interface" );
+        exit( -1 );
+    }
+    buf[n] = 0;
+    rx1 = strtol( buf, NULL, 10 );
+    if( ( n = pread( txfile, buf, 1023, 0 ) ) < 0 ) {
+        print_error( "Could not read from write statistics for interface" );
+        exit( -1 );
+    }
+    buf[n] = 0;
+    tx1 = strtol( buf, NULL, 10 );
+
+    if( gettimeofday( &tv1, NULL ) < 0 ) {
+        print_error( "Could not get the time of day" );
+        exit( -1 );
+    }
+    tv1.tv_usec -= 1000; // Offset for correct first timing
+
+    // Mark statistics log
+    if( write( statfile, buf2, strlen( buf2 ) ) < 0 ) {
+        print_error( "Could not write to statistics file" );
+        exit( -1 );
+    }
+
+    // Mark statistics version
+    write( statfile, "1\n", 2 );
+
+    // Prepare buf2 for more efficient usage down below
+    buf2[0] = 'a';
+    buf2[9] = 'b';
+    buf2[18] = 'c';
+    buf2[27] = 'd';
+    buf2[36] = 'e';
+    buf2[41] = '\n';
+    buf2[42] = 0;
+
+    // Start measurements
+    while( !end_loop ) {
+        // Get measurements
+        getrusage( RUSAGE_SELF, &ru2 );
+        microsuser = (uint32_t)( ( ru2.ru_utime.tv_sec - ru1.ru_utime.tv_sec ) * 1000000 + ( ru2.ru_utime.tv_usec - ru1.ru_utime.tv_usec ) );
+        microskernel = (uint32_t)( ( ru2.ru_stime.tv_sec - ru1.ru_stime.tv_sec ) * 1000000 + ( ru2.ru_stime.tv_usec - ru1.ru_stime.tv_usec ) );
+        ru1.ru_utime.tv_sec = ru2.ru_utime.tv_sec;
+        ru1.ru_utime.tv_usec = ru2.ru_utime.tv_usec;
+        ru1.ru_stime.tv_sec = ru2.ru_stime.tv_sec;
+        ru1.ru_stime.tv_usec = ru2.ru_stime.tv_usec;
+
+        n = pread( rxfile, buf, 1023, 0 );
+        buf[n] = 0;
+        rx2 = strtol( buf, NULL, 10 );
+        bytesread = rx2 - rx1;
+        rx1 = rx2;
+
+        n = pread( txfile, buf, 1023, 0 );
+        buf[n] = 0;
+        tx2 = strtol( buf, NULL, 10 );
+        byteswritten = tx2 - tx1;
+        tx1 = tx2;
+
+        // Transform data to a fast but robust format to be saved
+        microsuser = htobe32(microsuser);
+        microskernel = htobe32(microskernel);
+        bytesread = htobe32(bytesread);
+        byteswritten = htobe32(byteswritten);
+        sleepytime16 = htobe16((uint16_t)sleepytime);
+
+        buf2[1] = ( microsuser_p[0] >> 4 ) | 64;
+        buf2[2] = ( microsuser_p[0] & 15 ) | 64;
+        buf2[3] = ( microsuser_p[1] >> 4 ) | 64;
+        buf2[4] = ( microsuser_p[1] & 15 ) | 64;
+        buf2[5] = ( microsuser_p[2] >> 4 ) | 64;
+        buf2[6] = ( microsuser_p[2] & 15 ) | 64;
+        buf2[7] = ( microsuser_p[3] >> 4 ) | 64;
+        buf2[8] = ( microsuser_p[3] & 15 ) | 64;
+
+        buf2[10] = ( microskernel_p[0] >> 4 ) | 64;
+        buf2[11] = ( microskernel_p[0] & 15 ) | 64;
+        buf2[12] = ( microskernel_p[1] >> 4 ) | 64;
+        buf2[13] = ( microskernel_p[1] & 15 ) | 64;
+        buf2[14] = ( microskernel_p[2] >> 4 ) | 64;
+        buf2[15] = ( microskernel_p[2] & 15 ) | 64;
+        buf2[16] = ( microskernel_p[3] >> 4 ) | 64;
+        buf2[17] = ( microskernel_p[3] & 15 ) | 64;
+
+        buf2[19] = ( bytesread_p[0] >> 4 ) | 64;
+        buf2[20] = ( bytesread_p[0] & 15 ) | 64;
+        buf2[21] = ( bytesread_p[1] >> 4 ) | 64;
+        buf2[22] = ( bytesread_p[1] & 15 ) | 64;
+        buf2[23] = ( bytesread_p[2] >> 4 ) | 64;
+        buf2[24] = ( bytesread_p[2] & 15 ) | 64;
+        buf2[25] = ( bytesread_p[3] >> 4 ) | 64;
+        buf2[26] = ( bytesread_p[3] & 15 ) | 64;
+
+        buf2[28] = ( byteswritten_p[0] >> 4 ) | 64;
+        buf2[29] = ( byteswritten_p[0] & 15 ) | 64;
+        buf2[30] = ( byteswritten_p[1] >> 4 ) | 64;
+        buf2[31] = ( byteswritten_p[1] & 15 ) | 64;
+        buf2[32] = ( byteswritten_p[2] >> 4 ) | 64;
+        buf2[33] = ( byteswritten_p[2] & 15 ) | 64;
+        buf2[34] = ( byteswritten_p[3] >> 4 ) | 64;
+        buf2[35] = ( byteswritten_p[3] & 15 ) | 64;
+
+        buf2[37] = ( sleepytime16_p[0] >> 4 ) | 64;
+        buf2[38] = ( sleepytime16_p[0] & 15 ) | 64;
+        buf2[39] = ( sleepytime16_p[1] >> 4 ) | 64;
+        buf2[40] = ( sleepytime16_p[1] & 15 ) | 64;
+
+        // Write line of statistics
+        write( statfile, buf2, 42 );
+        
+        // Sleep until next timeslot (1000 Hz)
+        tv1.tv_usec += 1000;
+        if( tv1.tv_usec > 1000000 ) {
+            tv1.tv_usec -= 1000000;
+            tv1.tv_sec++;
+        }
+        gettimeofday( &tv2, NULL );
+        sleepytime = 1000 - ((tv2.tv_sec - tv1.tv_sec) * 1000000 + (tv2.tv_usec - tv1.tv_usec));
+        if( sleepytime > 0 )
+            usleep( sleepytime );
+    }
+
+    close( rxfile );
+    close( txfile );
+    close( statfile );
+
+    return NULL;
+}
+
 int main (int argc, char** argv) {
     // nodht TODO
     // old: daemon, debug, progress, http
@@ -70,6 +293,8 @@ int main (int argc, char** argv) {
         { "multiply",   required_argument,  0, '*' },
         { "many",       required_argument,  0, '#' },
         { "offset",     required_argument,  0, 'o' },
+        { "stat",       required_argument,  0, '@' },
+        { "iface",      required_argument,  0, 'i' },
 
         {0, 0, 0, 0}
     };
@@ -90,6 +315,12 @@ int main (int argc, char** argv) {
     int offset = 1;
     // Number of seeds to start
     int seedcount = 1;
+    // Name of the interface to be watches
+    char* interface = 0;
+    // Name of the statistics file
+    char* statistics = 0;
+    // Array with the addresses of the former two
+    char** statargs[] = { &statistics, &interface };
     
     // Initialize the library
     LibraryInit();
@@ -140,6 +371,12 @@ int main (int argc, char** argv) {
             case 'o' : // --offset
                 offset = readPositiveIntArg();
                 break;
+            case '@' : // --stat
+                statistics = strdup( optarg );
+                break;
+            case 'i' : // --iface
+                interface = strdup( optarg );
+                break;
             case '?' :
                 printf( "libswift command line test program\n" );
                 printf( "Usage: %s action [options]\n", argv[0] );
@@ -157,6 +394,8 @@ int main (int argc, char** argv) {
                 printf( "--multiply           Fake seeding more data by virtually multiplying the file a given number of times (positive integer, defaults to 1)\n" );
                 printf( "--many               Instead of seeding 1 file, seed the given file a given number of times, each next seed starting at +offset from the previous (positive integer, defaults to 1)\n" );
                 printf( "--offset             The offset for each next seed (positive integer, defaults to 1)\n" );
+                printf( "--stat               Write statistics to the specified file (filename, defaults to no statistics)\n" );
+                printf( "--iface              Interface to be watched for network traffic for the statistics (filename, required when --stat is present)\n" );
                 return 0;
         }
     }
@@ -177,6 +416,10 @@ int main (int argc, char** argv) {
         if( !filename )
             quit( "--file required when seeding\n" );
     }
+
+    // Check statistics, if applicable
+    if( statistics && !interface )
+        quit( "--iface is required when --stat is given\n" );
 
     // Bind to port
     int socket;
@@ -244,6 +487,36 @@ int main (int argc, char** argv) {
     if( sigaction( SIGINT, &signal_action, 0 ) )
         quit( "could not register signal handler for SIGINT: %s\n", strerror( errno ) );
 
+    // If needed, fire off statistics thread
+    pthread_t child;
+    if( statistics ) {
+        pthread_attr_t attr;
+        struct sched_param sp;
+        bzero( &sp, sizeof( struct sched_param ) );
+        sp.sched_priority = 1;
+        if( errno = pthread_attr_init( &attr ) ) {
+            print_error( "Could not create statistics thread attributes" );
+            exit(-1);
+        }
+        if( errno = pthread_attr_setdetachstate( &attr, PTHREAD_CREATE_JOINABLE ) ) {
+            print_error( "Could not set statistics thread detach state" );
+            exit(-1);
+        }
+        if( errno = pthread_attr_setschedpolicy( &attr, SCHED_FIFO ) ) {
+            print_error( "Could not set statistics thread scheduling policy" );
+            exit(-1);
+        }
+        if( pthread_attr_setschedparam( &attr, &sp ) ) {
+            print_error( "Could not set statistics thread scheduling priority" );
+            exit(-1);
+        }
+        if( errno = pthread_create( &child, &attr, statisticsThread, statargs ) ) {
+            print_error( "Could not create statistics thread" );
+            exit(-1);
+        }
+        pthread_attr_destroy( &attr );
+    }
+
     // Time at start
     tint end_time = NOW + (wait_time * TINT_SEC);
 
@@ -258,6 +531,7 @@ int main (int argc, char** argv) {
         while( ( !wait_time || end_time > NOW ) && !end_loop )
             swift::Loop( TINT_SEC );
     }
+    end_loop = true;
    
     // Cleanup
     for( i = 0; i < seedcount; i++ ) {
@@ -270,6 +544,8 @@ int main (int argc, char** argv) {
     // Shutdown library
     swift::Shutdown();
     
+    if( statistics )
+        pthread_join( child, NULL );
 
     return 0;
 }
